f21f1698fd6ce19f2307bd562f500cb893f6739e
rafaelcrespocrespo <racrecre@gmail.com>
2026-02-17 18:20:08 +0100

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // <-- Tailwind import
+import './index.css'; // <- aquí importa Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
